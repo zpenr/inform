4f8c3457302e28a5d61d98e0057f7163b79ada53
Myasnikov Vadim <zpenr@bk.ru>
2026-04-06 18:15:00 +0800

    feat: add CASE, subauery and CTE

--- DB_Students/db_work.py
+++ DB_Students/db_work.py
@@ -175,7 +175,98 @@ class Queries:
             """
             cursor.execute(query)
             return cursor.fetchall()
+    
+    @staticmethod
+    def marks(db_name: str|Path) -> list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            query = """SELECT
+                s.patronymic,
+                s.name,
+                n.name AS direction,
+                s.avg_mark,
+                CASE
+                WHEN s.avg_mark >= 4.5 THEN 'Отлично'
+                WHEN s.avg_mark >= 3.5 THEN 'Хорошо'
+                WHEN s.avg_mark >= 2.5 THEN 'Удовлетворительно'
+                ELSE 'Неудовлетворительно'
+                END AS mark_status
+                FROM students AS s
+                JOIN directions AS n ON s.id_direction = n.id_direction;"""
+            cursor.execute(query)
+            return cursor.fetchall()
+
+    @staticmethod
+    def direction_verdict(db_name: str|Path) ->list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            query = """SELECT
+                s.patronymic,
+                s.name,
+                n.name AS direction,
+                s.avg_mark,
+                CASE
+                WHEN n.name = 'Информационные системы' THEN 'Крутой чел'
+                ELSE 'Ну такой себе тип, сомнительный'
+                END AS verdict
+                FROM students AS s
+                JOIN directions AS n ON s.id_direction = n.id_direction;"""
+            cursor.execute(query)
+            return cursor.fetchall()
         
+    @staticmethod
+    def good_students(db_name: str|Path) ->list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            subquery = """SELECT AVG(avg_mark) FROM students"""
+            query = f"""SELECT *
+                FROM students AS s
+                WHERE s.avg_mark >= ({subquery});"""
+            cursor.execute(query)
+            return cursor.fetchall()
+        
+    @staticmethod
+    def students_except_direction(db_name: str|Path, direction: str) -> list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            subquery = """SELECT d.id_direction FROM directions AS d WHERE d.name = ?"""
+            query = f"""SELECT *
+                FROM students AS s
+                WHERE s.id_direction NOT IN ({subquery});"""
+            cursor.execute(query, (direction, ))
+            return cursor.fetchall()
+
+    @staticmethod
+    def good_students_cte(db_name: str | Path) -> list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            query = """
+                WITH avg_score AS (
+                    SELECT AVG(avg_mark) AS average FROM students
+                )
+                SELECT s.*
+                FROM students AS s, avg_score
+                WHERE s.avg_mark >= avg_score.average;
+            """
+            cursor.execute(query)
+            return cursor.fetchall()
+
+
+    @staticmethod
+    def students_except_direction_cte(db_name: str | Path, direction: str) -> list[tuple]:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            query = """
+                WITH target_direction AS (
+                    SELECT d.id_direction FROM directions AS d WHERE d.name = ?
+                )
+                SELECT s.*
+                FROM students AS s
+                WHERE s.id_direction NOT IN (SELECT id_direction FROM target_direction);
+            """
+            cursor.execute(query, (direction,))
+            return cursor.fetchall()
+    
 def data_to_table(data: dict[str, list], table_name: str, db_path: str|Path) -> None:
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
--- DB_Students/main.py
+++ DB_Students/main.py
@@ -7,10 +7,11 @@ db_work.Tables.create_all(db_path)
 
 DataLoader.load_files_to_db()
 
-print(db_work.Queries.all_students(db_path))
-print(db_work.Queries.students_num_by_directions(db_path))
-print(db_work.Queries.students_num_by_edc_types(db_path))
-print(db_work.Queries.marks_by_directions(db_path))
-print(db_work.Queries.best_students(db_path, "Информационные системы"))
-print(db_work.Queries.with_same_name(db_path))
-print(db_work.Queries.full_match(db_path))
+print(db_work.Queries.marks(db_path), '\n')
+print(db_work.Queries.direction_verdict(db_path), '\n')
+
+print(db_work.Queries.good_students(db_path), '\n')
+print(db_work.Queries.students_except_direction(db_path, "Дизайн"), '\n')
+
+print(db_work.Queries.good_students_cte(db_path), '\n')
+print(db_work.Queries.students_except_direction_cte(db_path, "Дизайн"), '\n')
